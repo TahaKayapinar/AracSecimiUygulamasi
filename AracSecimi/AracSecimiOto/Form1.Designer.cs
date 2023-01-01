240f5eb0df57cd7ffb57dd469215a6ca59c3fa0d
tahakayapinar <tahakayapinar@gmail.com>
2023-01-01 21:14:41 +0300

    AracSecimUygulamasi

---
+++ AracSecimi/AracSecimiOto/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AracSecimiOto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRuhsatNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMarka = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbModel = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbYakitTipi = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbKasaTipi = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtKilometresi = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFiyati = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ruhsat No";
+            // 
+            // txtRuhsatNo
+            // 
+            this.txtRuhsatNo.Location = new System.Drawing.Point(100, 6);
+            this.txtRuhsatNo.Name = "txtRuhsatNo";
+            this.txtRuhsatNo.Size = new System.Drawing.Size(208, 20);
+            this.txtRuhsatNo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marka";
+            // 
+            // cmbMarka
+            // 
+            this.cmbMarka.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMarka.FormattingEnabled = true;
+            this.cmbMarka.Location = new System.Drawing.Point(100, 37);
+            this.cmbMarka.Name = "cmbMarka";
+            this.cmbMarka.Size = new System.Drawing.Size(208, 21);
+            this.cmbMarka.TabIndex = 3;
+            this.cmbMarka.SelectedIndexChanged += new System.EventHandler(this.cmbMarka_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Model";
+            // 
+            // cmbModel
+            // 
+            this.cmbModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModel.FormattingEnabled = true;
+            this.cmbModel.Location = new System.Drawing.Point(100, 75);
+            this.cmbModel.Name = "cmbModel";
+            this.cmbModel.Size = new System.Drawing.Size(208, 21);
+            this.cmbModel.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Yakıt Tipi";
+            // 
+            // cmbYakitTipi
+            // 
+            this.cmbYakitTipi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbYakitTipi.FormattingEnabled = true;
+            this.cmbYakitTipi.Location = new System.Drawing.Point(100, 112);
+            this.cmbYakitTipi.Name = "cmbYakitTipi";
+            this.cmbYakitTipi.Size = new System.Drawing.Size(208, 21);
+            this.cmbYakitTipi.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Kasa Tipi";
+            // 
+            // cmbKasaTipi
+            // 
+            this.cmbKasaTipi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKasaTipi.FormattingEnabled = true;
+            this.cmbKasaTipi.Location = new System.Drawing.Point(431, 37);
+            this.cmbKasaTipi.Name = "cmbKasaTipi";
+            this.cmbKasaTipi.Size = new System.Drawing.Size(208, 21);
+            this.cmbKasaTipi.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(343, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Kilometresi";
+            // 
+            // txtKilometresi
+            // 
+            this.txtKilometresi.Location = new System.Drawing.Point(431, 76);
+            this.txtKilometresi.Name = "txtKilometresi";
+            this.txtKilometresi.Size = new System.Drawing.Size(208, 20);
+            this.txtKilometresi.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(343, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Fiyatı";
+            // 
+            // txtFiyati
+            // 
+            this.txtFiyati.Location = new System.Drawing.Point(431, 109);
+            this.txtFiyati.Name = "txtFiyati";
+            this.txtFiyati.Size = new System.Drawing.Size(208, 20);
+            this.txtFiyati.TabIndex = 1;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(346, 4);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 4;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(15, 160);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(155, 23);
+            this.btnKaydet.TabIndex = 4;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(190, 160);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(155, 23);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 216);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 234);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.cmbKasaTipi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbYakitTipi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbModel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbMarka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFiyati);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtKilometresi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtRuhsatNo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRuhsatNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMarka;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbModel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbYakitTipi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbKasaTipi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKilometresi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFiyati;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ AracSecimi/AracSecimiOto/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AracSecimiOto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            FillKasaTipi();
+            FillYakitTipi();
+            FillMarka();
+            FillDataGridView();
+        }
+
+        private void FillDataGridView()
+        {
+            dataGridView1.ColumnCount = 7;
+            dataGridView1.Columns[0].Name = "Ruhsat No";
+            dataGridView1.Columns[1].Name = "Marka";
+            dataGridView1.Columns[2].Name = "Model";
+            dataGridView1.Columns[3].Name = "Yakıt Tipi";
+            dataGridView1.Columns[4].Name = "Kasa Tipi";
+            dataGridView1.Columns[5].Name = "Kilometresi";
+            dataGridView1.Columns[6].Name = "Fiyatı";
+        }
+
+        private void FillMarka()
+        {
+            cmbMarka.Items.Clear();
+
+            cmbMarka.Items.Add("KIA");
+            cmbMarka.Items.Add("Toyota");
+            cmbMarka.Items.Add("Peugeot");
+            cmbMarka.Items.Add("Honda");
+            cmbMarka.Items.Add("Opel");
+            cmbMarka.Items.Add("Seat");
+            cmbMarka.Items.Add("BMW");
+            cmbMarka.Items.Add("Mercedes");
+            cmbMarka.Items.Add("Volkswagen");
+            cmbMarka.Items.Add("Volvo");
+        }
+
+        private void FillYakitTipi()
+        {
+            cmbYakitTipi.Items.Clear();
+
+            cmbYakitTipi.Items.Add("Benzinli");
+            cmbYakitTipi.Items.Add("Dizel");
+            cmbYakitTipi.Items.Add("Benzin + LPG");
+            cmbYakitTipi.Items.Add("Elektrikli");
+            cmbYakitTipi.Items.Add("Hibrit");
+        }
+
+        private void FillKasaTipi()
+        {
+            cmbKasaTipi.Items.Clear();
+
+            cmbKasaTipi.Items.Add("Sedan");
+            cmbKasaTipi.Items.Add("Hatcback");
+            cmbKasaTipi.Items.Add("Arazi-SUV");
+            cmbKasaTipi.Items.Add("Coupe");
+            cmbKasaTipi.Items.Add("Cabrio");
+        }
+
+        private void cmbMarka_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbModel.Items.Clear();
+
+            string marka = cmbMarka.SelectedItem.ToString();
+
+            if (marka == "KIA")
+            {
+                string[] model = { "Sportage", "Ceed", "Sorento" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Toyota")
+            {
+                string[] model = { "Auris", "Corolla" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Peugeot")
+            {
+                string[] model = { "301", "206", "207", "407", "508" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Honda")
+            {
+                string[] model = { "Civic", "Accord" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Opel")
+            {
+                string[] model = { "Corsa", "Astra", "Meriva", "Vectra" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Seat")
+            {
+                string[] model = { "Leon", "Ibıza" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "BMW")
+            {
+                string[] model = { "5.20", "3.20", "3.16", "4.20", "X5", "X6" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Mercedes")
+            {
+                string[] model = { "E200", "C200", "CLK200", "S500" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Volkswagen")
+            {
+                string[] model = { "Polo", "Golf", "Passat", "Jetta" };
+                cmbModel.Items.AddRange(model);
+            }
+
+            if (marka == "Volvo")
+            {
+                string[] model = { "V40", "S60", "XC90" };
+                cmbModel.Items.AddRange(model);
+            }
+
+
+        }
+
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+
+            string[] aracBilgileri = new string[]
+            {
+                txtRuhsatNo.Text,
+                cmbMarka.Text, cmbModel.Text,
+                cmbYakitTipi.Text,
+                cmbKasaTipi.Text,
+                txtKilometresi.Text,
+                txtFiyati.Text
+            };
+
+            dataGridView1.Rows.Add(aracBilgileri);
+
+            FormClear();
+
+        }
+
+        private void FormClear()
+        {
+            txtRuhsatNo.Clear();
+            cmbMarka.Items.Clear();
+            cmbModel.Items.Clear();
+            cmbYakitTipi.Items.Clear();
+            cmbKasaTipi.Items.Clear();
+            txtKilometresi.Text = "";
+            txtFiyati.Clear();
+
+            FillKasaTipi();
+            FillYakitTipi();
+            FillMarka();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            {
+                if (dataGridView1.CurrentRow.Cells[0].Value != null)
+                    dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                else MessageBox.Show("Silmek için önce bir kayıt seçiniz");
+
+            }
+        }
+    }
+}
